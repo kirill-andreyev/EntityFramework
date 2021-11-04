67c42eb953a5336b5133c3bf6ed068c476449e05
Kirill <kirilland02@gmail.com>
2021-11-04 11:31:25 +0300

    Fixed Lab1, but not working

---
+++ EntityFrameworkLab1/ConsoleApp1/Models/JournalPublication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLAB1
+{
+    public class JournalPublication
+    {
+        public int JournalPublicationId { get; set; }
+        public int WorkId { get; set; }
+        public virtual Work Work { get; set; }
+        public string JournalName { get; set; }
+        public int Release { get; set; }
+        public DateTime PubDate { get; set; }
+        public int Pages { get; set; }
+        public string Quality { get; set; }
+    }
+}
---
+++ EntityFrameworkLab1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace EFLAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Author author = new Author { AuthorName = "TestAuthor1", Birth = new DateTime(1991, 1, 1), OriginalName = "Testing" };
+                Author author2 = new Author { AuthorName = "TestAuthor2", Birth = new DateTime(1996, 1, 23), OriginalName = "Testing-2" };
+
+                db.Authors.AddRange(author, author2);
+                db.SaveChanges();
+            }
+        }
+    }
+}
